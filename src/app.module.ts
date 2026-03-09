279bc94b44246e7788507a50a739de5067e4ba4d
Toushik-Ahmed <Pronay5271@gmail.com>
2026-03-09 12:13:44 +0600

    feat: add environment configuration and database connection
    - Updated package.json to include cross-env for environment management and added necessary dependencies for configuration and TypeORM.
    - Modified app.module.ts to integrate ConfigModule for environment variable loading and TypeOrmModule for database connection using configuration values.
    - Enhanced app.service.ts to log the server port from environment variables.
    - Removed baseUrl from tsconfig.json for cleaner configuration.
    - Added .development.env file with database connection details and server port.
    - Created user.dto.ts file for future user-related data transfer objects.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('Loading file:', `.${process.env.NODE_ENV}.env`);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log('DB_USER:', configService.get('DB_USER'));
+        console.log('DB_PASSWORD:', configService.get('DB_PASSWORD'));
+        console.log('DB_HOST:', configService.get('DB_HOST'));
+        console.log('DB_NAME:', configService.get('DB_NAME'));
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          autoloadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist/config.service';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {
+    const port = this.configService.get<number>('PORT');
+    console.log('server is running on port ' + port);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
